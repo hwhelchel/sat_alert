c904b3dccf9b02c7ca7be1afbf257486401687ea
Anne Willborn <awillborn@gmail.com>
2014-04-12 16:30:15 -0700

    Calculate distance and direction, ajax callbacks
    Add ajax callback for success --
    Calculate distance from user
    If user is close to ISS, calculate cardinal direction from user and write message

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -7,6 +7,77 @@ Pebble.addEventListener("ready",
         };
     });
 
+function okCallback(data){
+    lat = data['iss_position']['latitude'];
+    lon = data['iss_position']['longitude'];
+    var userCoords = user.getLocation()
+    distanceToISS = getDistanceInKilometers(lat, lon, userCoords.latitude, userCoords.longitude);
+    if (isClose(distanceToISS)) {
+        directionDegree = getDirectionDegree(userCoords.latitude, userCoords.longitude, lat, lon);
+        cardinalDirection = getCardinalDirection(directionDegree)
+        var message = "Look up space cadet! The ISS is to the" + cardinalDirection
+    }
+}
+
+function errorCallback(){
+    //ignore
+}
+
+Number.prototype.toRad = function() {
+   return this * Math.PI / 180;
+}
+
+Number.prototype.toDegree = function() {
+   return this / Math.PI * 180;
+}
+
+function getDistanceInKilometers(lat1, lon1, lat2, lon2){
+  var dLat = (lat2-lat1).toRad();
+  var dLon = (lon2-lon1).toRad();
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+    Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = 6371 * c;
+  return d;
+}
+
+var minimumDistance = 400 //kilometers
+function isClose(distanceToISS){
+  return distanceToISS < minimumDistance
+}
+
+function getDirectionDegree(lat1, lon1, lat2, lon2){
+  lat1 = lat1.toRad()
+  lat2 = lat2.toRad()
+  var lonDiff = (lon2 - lon1).toRad();
+  var y = Math.sin(lonDiff) * Math.cos(lat2)
+  var x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(lonDiff);
+  return (Math.atan2(y, x).toDegree() + 360) % 360;
+}
+
+function getCardinalDirection(directionDegree){
+  if(directionDegree < 22.5){
+    return "N"
+  } else if (directionDegree < 67.5){
+    return "NE"
+  } else if (directionDegree < 112.5){
+    return "E"
+  } else if (directionDegree < 157.5){
+    return "SE"
+  } else if (directionDegree < 202.5){
+    return "S"
+  } else if (directionDegree < 247.5){
+    return "SW"
+  } else if (directionDegree < 292.5){
+    return "W"
+  } else if (directionDegree < 337.5){
+    return "NW"
+  } else {
+    return "N"
+  }
+}
+
 Ajax = (function() {
     var requestObject = new XMLHttpRequest();
     var _formatData = function(data) {
@@ -38,4 +109,6 @@ Ajax = (function() {
             _request(data, okCallback, errorCallback);
         }
     };
+
+
 }());
