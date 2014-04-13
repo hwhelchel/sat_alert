0d51d1176da6dc96712fecd2a99aa3e22546626a
Quentin Devauchelle <quentin@devauchelle.eu>
2014-04-12 17:40:18 -0700

    Add the view

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,12 +1,3 @@
-Pebble.addEventListener("ready",
-    function(e) {
-        console.log("Hello world! - Sent from your javascript application.");
-        var data = {
-            url: "http://api.open-notify.org/iss-now.json",
-            type: "GET"
-        };
-    });
-
 Ajax = (function() {
     var requestObject = new XMLHttpRequest();
     var _formatData = function(data) {
@@ -39,3 +30,44 @@ Ajax = (function() {
         }
     };
 }());
+
+var View = function() {
+    this.messages = {
+        iss: {
+            title: "ISS Alert",
+            visibility: {
+                "true": function(direction) {
+                    return "The ISS is now visible on the " + direction + "!, take a moment to watch the sky, take a picture, and tweet it to #ISeeTheISS!";
+                },
+                "false": function() {
+                    return "The ISS is no longer visible around you!";
+                }
+            }
+        }
+    };
+};
+
+View.prototype = {
+    notify: function(object, visibility, direction) {
+        object = object.toLowerCase();
+        visibility = visibility.toString();
+        var messages = this.messages[object];
+
+        var title = messages.title;
+        var text = messages.visibility[visibility](direction);
+        Pebble.showSimpleNotificationOnPebble(title, text);
+    }
+};
+
+
+
+
+
+
+// Sets everything in motion
+Pebble.addEventListener("ready",
+    function(e) {
+        console.log("Hello world! - Sent from your javascript application.");
+        var view = new View();
+        view.notify("ISS", true, "North");
+    });
