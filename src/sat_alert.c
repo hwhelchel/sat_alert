bedb82f07dbf7cc1600dd5b9c23b18c42fe33e19
Quentin Devauchelle <quentin@devauchelle.eu>
2014-04-13 14:38:31 -0700

    Add an inbox app msg receiver

--- src/sat_alert.c
+++ src/sat_alert.c
@@ -6,6 +6,52 @@ static TextLayer *clock_layer;
 static AppTimer *timer;
 static uint32_t polling_frequency;
 
+static void set_info_text(char text[]){
+  text_layer_set_text(info_layer, text);
+}
+
+// Makes time from seconds
+static void handle_second_tick(struct tm* tick_time, TimeUnits units_changed) {
+
+  static char time_text[] = "00:00:00"; // Needs to be static because it's used by the system later.
+
+
+  strftime(time_text, sizeof(time_text), "%T", tick_time);
+  text_layer_set_text(clock_layer, time_text);
+}
+
+
+static void set_clock_layer(Layer *window_layer){
+  GRect bounds = layer_get_bounds(window_layer);
+
+  clock_layer = text_layer_create((GRect) { .origin = { 0, 10 }, .size = { bounds.size.w, 35 } });
+  text_layer_set_text_alignment(clock_layer, GTextAlignmentCenter);
+  text_layer_set_font(clock_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+
+  time_t now = time(NULL);
+  struct tm *current_time = localtime(&now);
+  handle_second_tick(current_time, SECOND_UNIT);
+  tick_timer_service_subscribe(SECOND_UNIT, &handle_second_tick);
+
+
+  layer_add_child(window_layer, text_layer_get_layer(clock_layer));
+}
+
+
+
+static void set_info_layer(Layer *window_layer){
+  info_layer = text_layer_create(GRect(9, 74, 144-10 /* width */, 168-70 /* height */));
+  text_layer_set_text_color(info_layer, GColorBlack);
+  text_layer_set_text_alignment(info_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(info_layer, GColorWhite);
+  text_layer_set_font(info_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+
+  static char text[] = "Matthew stinks, he did shower though, but well... Donate Shower Gel to @TheRealGlenn";
+  set_info_text(text);
+
+  layer_add_child(window_layer, text_layer_get_layer(info_layer));
+}
+
 static void ask_for_iss_location(void *data) {
   DictionaryIterator *iter;
   app_message_outbox_begin(&iter);
@@ -26,48 +72,15 @@ static void handle_out_failed(DictionaryIterator *failed, AppMessageResult reaso
   timer = app_timer_register(polling_frequency, ask_for_iss_location, NULL);
 }
 
-// Makes time from seconds
-static void handle_second_tick(struct tm* tick_time, TimeUnits units_changed) {
-
-  static char time_text[] = "00:00:00"; // Needs to be static because it's used by the system later.
-
-
-  strftime(time_text, sizeof(time_text), "%T", tick_time);
-  text_layer_set_text(clock_layer, time_text);
-}
-
-static void set_clock_layer(Layer *window_layer){
-  GRect bounds = layer_get_bounds(window_layer);
-
-  clock_layer = text_layer_create((GRect) { .origin = { 0, 10 }, .size = { bounds.size.w, 35 } });
-  text_layer_set_text_alignment(clock_layer, GTextAlignmentCenter);
-  text_layer_set_font(clock_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-
-  time_t now = time(NULL);
-  struct tm *current_time = localtime(&now);
-  handle_second_tick(current_time, SECOND_UNIT);
-  tick_timer_service_subscribe(SECOND_UNIT, &handle_second_tick);
-
-
-  layer_add_child(window_layer, text_layer_get_layer(clock_layer));
-}
-
-static void set_info_text(char text[]){
-  text_layer_set_text(info_layer, text);
-}
-
-static void set_info_layer(Layer *window_layer){
-  info_layer = text_layer_create(GRect(9, 74, 144-10 /* width */, 168-70 /* height */));
-  text_layer_set_text_color(info_layer, GColorBlack);
-  text_layer_set_text_alignment(info_layer, GTextAlignmentCenter);
-  text_layer_set_background_color(info_layer, GColorWhite);
-  text_layer_set_font(info_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
-
-  static char text[] = "Matthew stinks, he did shower though, but well... Donate Shower Gel to @TheRealGlenn";
+static void handle_in_received(DictionaryIterator *sent, void *context){
+  Tuple *test = dict_find(sent, 0);
+  static char text[140];
+  strncpy(text, test->value->cstring, 140);
   set_info_text(text);
-
-  layer_add_child(window_layer, text_layer_get_layer(info_layer));
 }
+
+
+
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
@@ -84,6 +97,7 @@ static void window_unload(Window *window) {
 static void app_message_init(void){
   app_message_register_outbox_sent(handle_out_sent);
   app_message_register_outbox_failed(handle_out_failed);
+  app_message_register_inbox_received(handle_in_received);
 
   const uint32_t inbound_size = 64;
   const uint32_t outbound_size = 64;
