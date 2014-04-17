c90912c1528ee3043dff912325098e2ccc4643d0
Quentin Devauchelle <quentin@devauchelle.eu>
2014-04-16 17:06:57 -0700

    Change to call right methods at the right time

--- src/message_handler.c
+++ src/message_handler.c
@@ -5,6 +5,7 @@ AppTimer *timer;
 uint32_t const inbound_size = 140;
 uint32_t const outbound_size = 64;
 uint32_t const polling_frequency = 60000; // 60 seconds
+char text[140];
 
 static void ask_for_iss_location(void *data) {
   DictionaryIterator *iter;
@@ -26,22 +27,25 @@ static void handle_out_failed(DictionaryIterator *failed, AppMessageResult reaso
   timer = app_timer_register(polling_frequency, ask_for_iss_location, NULL);
 }
 
+static void set_text_from_dict_tuple(DictionaryIterator *dict, uint16_t index){
+	Tuple *tuple = dict_find(dict, index);
+  strncpy(text, tuple->value->cstring, 140);
+}
+
 static void handle_in_received(DictionaryIterator *sent, void *context){
-  Tuple *test = dict_find(sent, 0);
-  static char text[140];
-  strncpy(text, test->value->cstring, 140);
+  set_text_from_dict_tuple(sent, 0);
   set_info_text(text);
   vibes_double_pulse();
 }
 
-void register_message_callback(void){
+void register_message_callbacks(void){
   app_message_register_outbox_sent(handle_out_sent);
   app_message_register_outbox_failed(handle_out_failed);
   app_message_register_inbox_received(handle_in_received);
 }
 
 void message_handler_init(void){
-  register_message_callback();
+  register_message_callbacks();
   app_message_open(inbound_size, outbound_size);
   ask_for_iss_location(NULL);
   poll_phone();
